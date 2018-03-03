ca98eedbee9382d46468560f45dc5b98bc1f3225
colemtg <cole-pierce@uiowa.edu>
2018-03-02 20:44:31 -0600

    Sends an Email Notification to new user

--- VotingApplication/Controllers/UserController.cs
+++ VotingApplication/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace VotingApplication.Controllers
 {
@@ -49,6 +51,29 @@ namespace VotingApplication.Controllers
 
             if (result.Succeeded)
             {
+                var fromAddress = new MailAddress("votingnotification6@gmail.com", "Voting System");
+                var toAddress = new MailAddress(email, username);
+                const string fromPassword = "Team6Admin";
+                const string subject = "Welcome Confirmation Email";
+                string body = "Hello " + username;
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(message);
+                }
                 await mSignInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction(nameof(Dashboard));
             }
