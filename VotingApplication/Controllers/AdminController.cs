24d13d23e7389c8c116fe3b13c7ce9e44c9910fd
Anes Kapo <anes-kapo@uiowa.edu>
2018-03-19 14:28:01 -0500

    Added Admin User Management
    Two seperate user management tables. Alternative does not yet work, first table is just cshtml template for future use, with a dropdown confirmation on button clicks.

---
+++ VotingApplication/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using VotingApplication.ViewModels;
+using System.Collections;
+
+namespace VotingApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        protected SignInManager<ApplicationUser> _SignInManager;
+        protected UserManager<ApplicationUser> _UserManager;
+
+        
+        public AdminController(
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            _SignInManager = signInManager;
+            _UserManager = userManager;
+        }
+        
+        
+        [Authorize]
+        public IActionResult Dashboard()
+        {
+            ViewData["UserName"] = HttpContext.User.Identity.Name;
+            return View("Dashboard/Index"); //Index view
+        }
+
+        public IActionResult UserManagement()
+        {
+            ViewData["UserName"] = HttpContext.User.Identity.Name;
+            return View("UserManagement/Index");
+        }
+
+        public IActionResult UserManagement2()
+        {
+            ViewData["UserName"] = HttpContext.User.Identity.Name;
+            return View("UserManagement2/Index");
+        }
+    }
+}
---
+++ VotingApplication/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotingApplication.ViewModels
+{
+    public class UserViewModel
+    
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+    }
+}
