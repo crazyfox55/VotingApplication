7cc3d0c1c15de082a7712959fa4a181288f92e22
colemtg <cole-pierce@uiowa.edu>
2018-03-31 19:26:41 -0500

    Started views/viewmodels for adding candidates and offices

---
+++ VotingApplication/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingApplication.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+        protected ApplicationDbContext _Context;
+        
+        public AdminController(ApplicationDbContext context)
+        {
+            _Context = context;
+        }
+        
+        [HttpGet]
+        public IActionResult AddOffice()
+        {
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult AddCandidate()
+        {
+            return View(); 
+        }
+    }
+}
