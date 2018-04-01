ae553459176e59d90ae6d86d241278af1ed9e3ce
David McDermott <crazyfox343@gmail.com>
2018-04-01 02:46:09 -0500

    Imported changes made in the Admin/Manager branch.
    Admin/Manager branch was too far from the master to merge, instead code was copied.
    I added some major changes. Accesses the database to poulate the table. Delete and Confirm-Delete buttons are linked together and call commands on the server but the backend ignores the request. Edit calls commands on the server but the backend ignores the request.
    Added styles to the table. Used danger buttons for the delete and confirm delete.
    
    Co-Authored-By: honestly7 <honestly7@users.noreply.github.com>

---
+++ VotingApplication/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VotingApplication.ViewModels;
+
+namespace VotingApplication.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+
+        protected ApplicationDbContext _Context;
+
+        public AdminController(
+            ApplicationDbContext context)
+        {
+            _Context = context;
+        }
+        
+        public IActionResult Dashboard()
+        {
+            ViewData["UserName"] = User.Identity.Name;
+            return View(); 
+        }
+
+        public IActionResult UserManagement(int page = 0, int usersPerPage = 5)
+        {
+            // max 50 users
+            usersPerPage = Math.Min(usersPerPage, 50);
+            // min 5 users
+            usersPerPage = Math.Max(usersPerPage, 5);
+
+            List<ManageUserViewModel> model = new List<ManageUserViewModel>();
+            ApplicationUser[] users = _Context.Users
+                .Skip(page * usersPerPage)
+                .Take(usersPerPage)
+                .ToArray();
+            for (int i = 0; i < users.Length; i++)
+            {
+                model.Add(new ManageUserViewModel(users[i]));
+            }
+
+            return View(model);
+        }
+
+        // needs to use the user manager to display a form where a user can be edited.
+        public IActionResult Edit(string Username)
+        {
+            return RedirectToAction(nameof(UserManagement));
+        }
+
+        // needs to use the user manager to delete a user.
+        public IActionResult Delete(string Username)
+        {
+            return RedirectToAction(nameof(UserManagement));
+        }
+    }
+}
