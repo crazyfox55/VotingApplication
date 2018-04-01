f6c9315ddc7452ac427f38c86226b6035caf3400
Junyan Yang <yangwu123789@gmail.com>
2018-03-31 20:49:11 -0500

    particially finished email notification and manager query

---
+++ VotingApplication/Controllers/ManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotingApplication.Controllers
+{
+    public class ManagerController : Controller
+    {
+        [HttpGet]
+        public IActionResult UserQuery()
+        {
+            return View();
+        }
+    }
+}
---
+++ VotingApplication/ViewModels/UserQueryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using VotingApplication.CustomAttributes;
+
+namespace VotingApplication.ViewModels
+{
+    public class UserQueryViewModel
+    {
+        [Required]
+        [StringLength(maximumLength: 256, ErrorMessage = "Maximum length of {0}")]
+        [Display(Name = "Query By")]
+        public string Option { get; set; }
+    }
+}
