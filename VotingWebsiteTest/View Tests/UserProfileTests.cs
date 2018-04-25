a2ad57afc4eaa0ca0077340c279a6f0b0fb022b6
colemtg <cole-pierce@uiowa.edu>
2018-04-25 18:34:51 -0500

    Starting on Profile tests

---
+++ VotingWebsiteTest/View Tests/UserProfileTests.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using Xunit;
+
+namespace VotingWebsiteTest.View_Tests
+{
+    public class UserProfileTests
+    {
+        //This should work for relative source, if not change to where the chromedriver is
+        ChromeDriver _chrome = new ChromeDriver((Directory.GetParent(Directory.GetCurrentDirectory())).Parent.Parent.FullName);
+
+        string user = "GCTZSBARUJ"; //set to one of script users
+
+        //ZLZJTCWVYN, DLVJEJZWCJ, UETJTLCKOS, RPSFWFCDDW
+        //MUZDSQNSND, CCPEWXTIPI, PQZJMBWJXM, NRRONNDZTX, GCTZSBARUJ, ANSEGITLRC, ZZZNXOQZGM, XYYGBDSFVO, NPNUKNEDYP, KRHQHKZYRQ, 
+        //OWHOWZYSSR, YBZIYLUGXG, RQZRJUYTVE, HCOOWWXDBT, FLMVXEPWRH, TQDLHCAFUK, CYIZRHJABE, BXTRSCERPY, BFNNSYVTTV, UKTXIPEOBN, QVBJZVBZQQ
+        [Fact]
+        public void NavigateToPage()
+        {
+            var username = user;
+            var password = "hello";
+            _chrome.Navigate().GoToUrl("http://localhost:5000/Authentication/Login");
+            _chrome.FindElementById("Username").SendKeys(username);
+            _chrome.FindElementById("Password").SendKeys(password);
+            _chrome.FindElementById("Login").Click();
+            Assert.Contains(username.ToLower(), _chrome.FindElementById("DashboardMsg").Text.ToLower());
+            _chrome.FindElementByLinkText("Profile").Click();
+            Assert.Contains("User Profile", _chrome.FindElementById("Profile").Text);
+
+            _chrome.Close();
+        }
+
+        [Fact]
+        public void ChangePasswordLinkWorks()
+        {
+            var username = user;
+            var password = "hello";
+            _chrome.Navigate().GoToUrl("http://localhost:5000/Authentication/Login");
+            _chrome.FindElementById("Username").SendKeys(username);
+            _chrome.FindElementById("Password").SendKeys(password);
+            _chrome.FindElementById("Login").Click();
+            Assert.Contains(username.ToLower(), _chrome.FindElementById("DashboardMsg").Text.ToLower());
+            _chrome.FindElementByLinkText("Profile").Click();
+            Assert.Contains("User Profile", _chrome.FindElementById("Profile").Text);
+
+            _chrome.FindElementByLinkText("Change Password").Click();
+            Assert.Contains("Change Password", _chrome.FindElementByClassName("form-signin-heading").Text);
+
+            _chrome.Close();
+        }
+
+        [Fact]
+        public void SecurityQuestionsLinkWorks()
+        {
+            var username = user;
+            var password = "hello";
+            _chrome.Navigate().GoToUrl("http://localhost:5000/Authentication/Login");
+            _chrome.FindElementById("Username").SendKeys(username);
+            _chrome.FindElementById("Password").SendKeys(password);
+            _chrome.FindElementById("Login").Click();
+            Assert.Contains(username.ToLower(), _chrome.FindElementById("DashboardMsg").Text.ToLower());
+            _chrome.FindElementByLinkText("Profile").Click();
+            Assert.Contains("User Profile", _chrome.FindElementById("Profile").Text);
+
+            _chrome.FindElementByLinkText("Add Security Questions").Click();
+            Assert.Contains("Add Security Questions", _chrome.FindElementByClassName("form-signin-heading").Text);
+
+            _chrome.Close();
+        }
+
+    }
+ }
